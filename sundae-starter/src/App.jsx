02a20cf49866b57d3fb0c9661ee732f71b114ca3
Sasha Pimenov <pimenovs33@gmail.com>
2024-10-03 19:49:29 +0300

    Added test for subtotals and code for entry and summary pages; added context (not in tests)

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,9 +1,15 @@
+import Container from "react-bootstrap/Container";
+import OrderEntry from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+
 function App() {
-  return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-    </div>
-  );
+	return (
+		<Container>
+			<OrderDetailsProvider>
+				<OrderEntry />
+			</OrderDetailsProvider>
+		</Container>
+	);
 }
 
 export default App;
