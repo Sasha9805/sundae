0a77f4330fc4387e9280d7e7ec018a4420e0135e
Sasha Pimenov <pimenovs33@gmail.com>
2024-10-05 18:03:55 +0300

    Added cancellation of request; see warning with act()

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -15,12 +15,23 @@ export default function Options({ optionType }) {
 
 	// optionType is 'scoops' or 'toppings' (enum)
 	useEffect(() => {
+		// create an abortController to attach to network request
+		const controller = new AbortController();
 		axios
-			.get(`http://localhost:3030/${optionType}`)
+			.get(`http://localhost:3030/${optionType}`, {
+				signal: controller.signal,
+			})
 			.then((response) => setItems(response.data))
 			.catch((error) => {
-				setError(true);
+				if (error.name !== "CnaceledError") {
+					setError(true);
+				}
 			});
+
+		// abort axios call on component unmount
+		return () => {
+			controller.abort();
+		};
 	}, [optionType]);
 
 	if (error) {
--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -66,12 +66,14 @@ test("Update topping subtotal when toppings change", async () => {
 
 describe("Grand total", () => {
 	test("grand total starts at $0.00", () => {
-		render(<OrderEntry />);
+		const { unmount } = render(<OrderEntry />);
 
 		const grandTotal = screen.getByRole("heading", {
 			name: /grand total: \$/i,
 		});
 		expect(grandTotal).toHaveTextContent("0.00");
+
+		unmount();
 	});
 	test("grand total updates properly if scoop is added first", async () => {
 		const user = userEvent.setup();
