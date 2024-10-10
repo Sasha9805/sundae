f1f9d2d4c01d776768f6b4eadc1c0faa24f8a753
Sasha Pimenov <pimenovs33@gmail.com>
2024-10-10 19:52:19 +0300

    Added test for Options - don't update scoops total if input is invalid

--- sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 
 test("Displays image for each scoop option from server", async () => {
@@ -13,7 +14,7 @@ test("Displays image for each scoop option from server", async () => {
 	expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
 
-test("Displays image for each topping oprion from server", async () => {
+test("Displays image for each topping option from server", async () => {
 	render(<Options optionType="toppings" />);
 
 	const toppingImages = await screen.findAllByRole("img", {
@@ -27,4 +28,27 @@ test("Displays image for each topping oprion from server", async () => {
 		"M&Ms topping",
 		"Hot fudge topping",
 	]);
+});
+
+test("Don't update total if scoops input is invalid", async () => {
+	const user = userEvent.setup();
+
+	render(<Options optionType="scoops" />);
+
+	const vanillaInput = await screen.findByRole("spinbutton", {
+		name: "Vanilla",
+	});
+	const scoopsTotal = screen.getByText("Scoops total: $0.00");
+
+	await user.clear(vanillaInput);
+	await user.type(vanillaInput, "2.5");
+	expect(scoopsTotal).toHaveTextContent("$0.00");
+
+	await user.clear(vanillaInput);
+	await user.type(vanillaInput, "100");
+	expect(scoopsTotal).toHaveTextContent("$0.00");
+
+	await user.clear(vanillaInput);
+	await user.type(vanillaInput, "-1");
+	expect(scoopsTotal).toHaveTextContent("$0.00");
 });
